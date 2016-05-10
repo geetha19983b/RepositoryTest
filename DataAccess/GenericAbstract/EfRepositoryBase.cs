347219f0fa8dd3439adc6c32c04da53bf66111a7
geetha19983b <geethainfy@gmaiil.com>
2016-05-10 17:03:33 +0530

    commit

---
+++ DataAccess/GenericAbstract/EfRepositoryBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.GenericInterface;
+using System.Data.Entity;
+
+namespace DataAccess.GenericAbstract
+{
+    public class EfRepositoryBase: IRepositoryBase
+    {
+        protected DbContext Context;
+
+        public EfRepositoryBase(DbContext dbCtx)
+        {
+            Context = dbCtx;
+        }
+        public int ExecuteScalar(string query)
+        {
+
+            return Context.Database.ExecuteSqlCommand(query);
+        }
+
+
+        public IEnumerable<T> ExecuteReader<T>(string query, object parameters)
+        {
+           
+            return Context.Database.SqlQuery<T>(query, parameters);
+        }
+    }
+}
---
+++ RepositoryTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccess.GenericAbstract;
+using RepositoryTest.Models;
+
+namespace RepositoryTest.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            EfRepositoryBase efrepobj = new EfRepositoryBase(new SampleEntities());
+            //efrepobj.ExecuteScalar("update [Employees] set EmpAlias3 = 'John' where EmpId = 1");
+
+            IEnumerable<Employee> emps = efrepobj.ExecuteReader<Employee>("spGetAllEmployees1", null);
+            var mods = emps;
+            return View(mods);
+        }
+	}
+}
