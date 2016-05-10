8356f28dbdc972a55bd76b0677e06941d208e3ae
geetha19983b <geethainfy@gmail.com>
2016-05-10 23:36:05 +0530

    Commit

--- RepositoryTest/Controllers/HomeController.cs
+++ RepositoryTest/Controllers/HomeController.cs
@@ -10,17 +10,53 @@ namespace RepositoryTest.Controllers
 {
     public class HomeController : Controller
     {
-       
+
+
+        EfRepositoryBase<Employee,SampleEntities> efrepobj = new EfRepositoryBase<Employee,SampleEntities>();
         //
         // GET: /Home/
         public ActionResult Index()
         {
-            EfRepositoryBase efrepobj = new EfRepositoryBase(new SampleEntities());
+            
             //efrepobj.ExecuteScalar("update [Employees] set EmpAlias3 = 'John' where EmpId = 1");
 
             IEnumerable<Employee> emps = efrepobj.ExecuteReader<Employee>("spGetAllEmployees1", null);
             var mods = emps;
+            ViewBag.EmpCount = efrepobj.ExecuteScalar<int>("select count(*) from Employee", null);
             return View(mods);
+        }
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                efrepobj.Insert(employee);
+                efrepobj.Save();
+                
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+
+            Employee emp = efrepobj.ExecuteReader<Employee>("spGetFilterEmployees @p0", id).Single();
+            return View(emp);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee emp)
+        {
+            string query = "update Employee set EmpAlias3 = '" + emp.EmpAlias3 + "' where EmpId = " + emp.EmpId;
+            efrepobj.ExecuteNonQuery("update Employee set EmpAlias3 = '" + emp.EmpAlias3 + "' where EmpId = " + emp.EmpId);
+            return RedirectToAction("Index");
         }
 	}
 }
--- RepositoryTest/Models/SampleEntities.Designer.cs
+++ RepositoryTest/Models/SampleEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Visual Studio 2013\Projects\RepositoryTest\RepositoryTest\Models\SampleEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Priya\Source\Repos\RepositoryTest\RepositoryTest\Models\SampleEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
